e5a72ea43711a671e3d42f0f27e75ce72882f40d
Felix <felix@angellecho.com>
2017-12-17 09:17:31 +0800

    单链表的头插法和尾插法建表以及整表删除

--- 01.c
+++ 01.c
@@ -8,7 +8,8 @@ typedef struct Node
 typedef struct Node* LinkList;
 
 
-//读取指定的元素
+//读取指定位置i的元素
+//时间复杂度为O(n)
 Status getElement(LinkList L,int i ,ElemType *e)
 {
   int j;
@@ -19,11 +20,56 @@ Status getElement(LinkList L,int i ,ElemType *e)
   while (p && j<i) {
     p = p->next; //指针移动
     ++j;
-    
   }
   if (!p || j>i)  {
     return ERROR;
   }
   *e = p->data;
   return OK;
+}
+//单链表的元素插入
+Status ListInsert(LinkList L,int i,ElementType e)
+{
+  int j;
+  LinkList p,a;
+  p = *L;
+  j = 1;
+  while (p && j<i) {
+    //指针循环遍历
+    p = p->next;
+    j++;
+
+  }
+  if(!p || j>i){
+    return ERROR;
+  }
+  s = (LinkList)malloc(sizeof(Node));//为插入的元素分配一个内存
+  s->data = e;
+
+  s->next = p->next;
+  p->next = s;
+
+  return OK;
+}
+
+//单链表的删除操作
+Status LinkDelete(LinkList L,int i,ElementType e){
+  int j;
+  LinkList p,q;
+  p = *L;
+  j = 1;
+
+  while (p->next && j<i) {
+    p = p->next;
+    ++j;
+  }
+  if(!(p->next) || j>i){
+    return ERROR;
+  }
+  q = p->next;
+  p->next = q->next;
+  *e = q->data;
+  free(q);
+  return OK;
+
 }
