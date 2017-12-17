8687c22080a85127cc8c3839deda308ee36b6c16
Felix <felix@angellecho.com>
2017-12-17 20:22:40 +0800

    双向链表实例

---
+++ 05.c
@@ -0,0 +1,70 @@
+/*
+实现用户输入一个数使得26个字母的顺序发生变化
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#define OK 1
+#define ERROR 0
+typedef char ElemType;
+typedef int Status;
+typedef struct DualNode
+{
+  ElemType data;
+  struct DualNode *prior;
+  struct DualNode *next;
+}DualNode,*DuLinkList;
+//初始化链表
+Status InitList(DuLinkList *L){
+  DualNode *p,*q;
+  int i;
+  *L = (DuLinkList)malloc(sizeof(DualNode));
+  if(!(*L)){
+    //内存分配失败
+    return ERROR;
+  }
+  (*L)->next = (*L)->prior = NULL;
+  p = (*L);
+  for(i = 0;i<26;i++){
+    q = (DualNode *)malloc(sizeof(DualNode));
+    if (!q) {
+      return ERROR;
+    }
+    q->data = 'A'+i;
+    q->prior = p;
+    q->next = p->next;
+    p->next = q;
+
+    p = q;
+  }
+  p->next = (*L)->next;
+  (*L)->next->prior = p;
+  return OK;
+}
+void Caesar(DuLinkList *L,int i){
+  if (i>0) {
+    do
+    {
+        (*L) = (*L)->next;
+    }
+    while (--i);
+  }
+  if (i<0) {
+    do {
+      (*L) = (*L)->next;
+    } while(++i);
+  }
+}
+int main(int argc, char const *argv[]) {
+  DuLinkList L;
+  int i,n;
+  InitList(&L);
+  printf("请输入一个整数：");
+  scanf("%d\n",&n );
+  printf("\n");
+  Caesar(&L,n);
+  for (i=0;i<26;i++) {
+    L = L->next;
+    printf("%c",L->data);
+  }
+  return 0;
+}
